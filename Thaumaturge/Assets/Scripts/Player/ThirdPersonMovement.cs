1d06dea3e3cf8fc5198d95fcf946b97dc85e9633
Carson McMahan <carson.mcmahan@gmail.com>
2023-02-15 00:12:40 -0700

    Player
    Player can now jump and move at the same time.

--- Thaumaturge/Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Thaumaturge/Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -42,25 +42,25 @@ public class ThirdPersonMovement : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
+        // Player movement
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+
+            // Player will sprint while holding shift
+            if (Input.GetKey(sprint))
+            {
+                controller.Move(moveDirection.normalized * speed * spritingMultipler * Time.deltaTime);
+            }
+        }
+
         if (controller.isGrounded)
         {
-            // Player movement
-            if (direction.magnitude >= 0.1f)
-            {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0f, angle, 0f); 
-
-                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
-
-                // Player will sprint while holding shift
-                if (Input.GetKey(sprint))
-                {
-                    controller.Move(moveDirection.normalized * speed * spritingMultipler * Time.deltaTime);
-                }
-            }
-
             // Player jumps
             if (Input.GetKeyDown(jump))
             {
